15eae6fd90510948dec6a12721d69b74b00a49b6
Lilou DUFAU <dufaulilou16@gmail.com>
2025-08-20 21:01:10 +0200

    ➕ Refactor layout structure by moving HTML from welcome.blade.php to app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- <link href="/src/style.css" rel="stylesheet"> --}}
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Lilou DUFAU</title>
+</head>
+<body class="font-[Poppins] h-screen overflow-hidden">
+    <main class="grid grid-cols-12 h-full">
+        {{-- section principale --}}
+         <section class="col-span-12 bg-gray-100 h-full flex flex-col">
+
+            {{-- header --}}
+            <header class="bg-gray-800 text-white p-4">
+                @include('layouts.partials.header')
+            </header>
+
+            {{-- contenu principal --}}
+            <div class="flex-1 pb-8 mx-4 my-2">
+                @yield('content')
+            </div>
+
+            {{-- footer --}}
+            <footer class="bg-gray-800 text-white p-4">
+                @include('layouts.partials.footer')
+            </footer>
+
+        </section>
+    </main>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <link href="/src/style.css" rel="stylesheet"> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1 class="text-3xl font-bold underline text-blue-500">
     Hello world!
     </h1>
-</body>
-</html>
+@endsection
